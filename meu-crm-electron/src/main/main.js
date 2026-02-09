3289b49ceb04bd1d7a0bbb764c335dcbc9431697
paulobarrozosf-hash <paulo.barrozosf@gmail.com>
2026-02-09 13:49:56 -0300

    Create main.js

---
+++ meu-crm-electron/src/main/main.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, '../preload/preload.js')
+    },
+    icon: path.join(__dirname, '../../assets/icons/icon.png')
+  });
+
+  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+
+  // Abre DevTools em modo desenvolvimento
+  if (process.argv.includes('--dev')) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// Exemplo de comunicação IPC (opcional)
+ipcMain.handle('get-app-version', () => {
+  return app.getVersion();
+});
